9d60f486167b4eab9cd148ab17f24748841b2ea6
Ryan Gilboy <rgilboy@chicagobooth.edu>
2023-04-18 06:12:08 +0000

    Finished

--- app/controllers/exchange_controller.rb
+++ app/controllers/exchange_controller.rb
@@ -22,6 +22,24 @@ class ExchangeController < ApplicationController
 
   def calculate
 
+    @ticker1 = params.fetch("ticker1")
+    @ticker2 = params.fetch("ticker2")
+
+    @currency_api_endpoint = "https://api.exchangerate.host/convert?from=#{@ticker1}&to=#{@ticker2}"
+
+    @raw_response = URI.open(@currency_api_endpoint).read
+
+    require "json"
+
+    @parsed_response = JSON.parse(@raw_response)
+
+    @query = @parsed_response.fetch("query")
+    @amount = @query.fetch("amount")
+
+    @info = @parsed_response.fetch("info")
+    @rate = @info.fetch("rate")
+
+
     render(:template => "calculation_page.html.erb")
   end
 
